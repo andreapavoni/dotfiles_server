9fb66bbf006b3c6abfcdb4fd0ce356206a01078e
Andrea Pavoni <andrea.pavoni@gmail.com>
2022-10-11 17:53:57 +0200

    finally fix zsh history with arrow keys

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -2,17 +2,13 @@
 # cat > /dev/null
 # And press it
 
-# lame workaround for history search
-bindkey "$key[Up]"  history-search-backward
-bindkey "$key[Down]" history-search-forward
-
 bindkey -e   # Default to standard vi bindings, regardless of editor string
 bindkey "^K"      kill-line                            # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a
 bindkey "^E"      end-of-line                          # ctrl-e
-# bindkey "^[[A"    history-beginning-search-backward    # up arrow
-# bindkey "^[[B"    history-beginning-search-forward     # down arrow
+bindkey "$key[Up]"  history-search-backward
+bindkey "$key[Down]" history-search-forward
 bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
