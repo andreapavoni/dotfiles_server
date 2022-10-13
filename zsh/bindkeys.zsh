c5d5e1241c5a66f408b2bac7c09b9fc9d7234adb
Andrea Pavoni <andrea.pavoni@gmail.com>
2022-10-13 16:19:45 +0200

    fix zsh history

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -7,8 +7,8 @@ bindkey "^K"      kill-line                            # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a
 bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "$key[Up]"  history-search-backward
-bindkey "$key[Down]" history-search-forward
+bindkey "$key[Up]"  history-beginning-search-backward
+bindkey "$key[Down]" history-beginning-search-forward
 bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
